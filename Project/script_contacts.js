b396e9902b697517afe226457971c761e6143266
Adema Shauenova <ademashauenova@gmail.com>
2023-10-22 20:26:51 +0600

    color change and alerts

--- Project/script_contacts.js
+++ Project/script_contacts.js
@@ -8,3 +8,31 @@ document.getElementById("sendButton").addEventListener("click", function() {
 
     document.getElementById("contactForm").reset();
 });
+
+function redirectWithAlert(link, destination) {
+        if (alert('You will be redirected to ' + destination + '.')) {
+            window.location.href = link;
+        }
+    }
+
+const instagramLink = document.getElementById('instagram-link');
+const tiktokLink = document.getElementById('tiktok-link');
+const telegramLink = document.getElementById('telegram-link');
+
+if (instagramLink) {
+    instagramLink.addEventListener('click', function () {
+        redirectWithAlert('https://instagram.com/kca_aitu?igshid=MzRlODBiNWFlZA==', 'Instagram');
+    });
+}
+
+if (tiktokLink) {
+    tiktokLink.addEventListener('click', function () {
+        redirectWithAlert('https://www.tiktok.com/@bultaoreune_kca?_t=8fiQ3v1ZEDG&_r=1', 'Tiktok');
+    });
+}
+
+if (telegramLink) {
+    telegramLink.addEventListener('click', function () {
+        redirectWithAlert('https://t.me/+UkXBxUkgeeNLQCAL', 'Telegram');
+    });
+}
