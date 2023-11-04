7c778e9b936f155777a66b0a53a862ace50ed7e7
NUKE05 <armornurik@gmail.com>
2023-11-04 22:51:59 +0600

    Update script_contacts.js

--- Project/script_contacts.js
+++ Project/script_contacts.js
@@ -1,13 +1,59 @@
-document.getElementById("sendButton").addEventListener("click", function() {
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var message = document.getElementById("message").value;
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const messageInput = document.getElementById('message');
+const sendButton = document.getElementById('sendButton');
 
-    var formData = "Name: " + name + "\nEmail: " + email + "\nMessage: " + message;
-    alert(formData);
+nameInput.addEventListener('blur', validateName);
+emailInput.addEventListener('blur', validateEmail);
+messageInput.addEventListener('blur', validateMessage);
 
-    document.getElementById("contactForm").reset();
-});
+function validateName() {
+    const nameValue = nameInput.value.trim();
+    if (nameValue === '') {
+        nameInput.classList.add('is-invalid');
+        nameInput.classList.remove('is-valid');
+    } else {
+        nameInput.classList.remove('is-invalid');
+        nameInput.classList.add('is-valid');
+    }
+}
+
+function validateEmail() {
+    const emailValue = emailInput.value.trim();
+    const emailRegex = /^[\w.%+-]+@[\w.-]+\.[A-Z]{2,}$/i;
+    if (!emailRegex.test(emailValue)) {
+        emailInput.classList.add('is-invalid');
+        emailInput.classList.remove('is-valid');
+    } else {
+        emailInput.classList.remove('is-invalid');
+        emailInput.classList.add('is-valid');
+    }
+}
+
+function validateMessage() {
+    const messageValue = messageInput.value.trim();
+    if (messageValue === '') {
+        messageInput.classList.add('is-invalid');
+        messageInput.classList.remove('is-valid');
+    } else {
+        messageInput.classList.remove('is-invalid');
+        messageInput.classList.add('is-valid');
+    }
+}
+
+sendButton.addEventListener('click', submitForm);
+
+function submitForm() {
+    const nameValue = nameInput.value.trim();
+    const emailValue = emailInput.value.trim();
+    const messageValue = messageInput.value.trim();
+
+    if (nameValue === '' || emailValue === '' || messageValue === '') {
+        alert('Please fill in all required fields.');
+    } else {
+        alert('Form submitted successfully!');
+    }
+}
 
 function redirectWithAlert(link, destination) {
         if (alert('You will be redirected to ' + destination + '.')) {
