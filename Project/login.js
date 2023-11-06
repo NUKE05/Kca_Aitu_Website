62a7144448633a0350d49e7c74b2fee4c078e87e
Nargiz Merlenkyzy <merlenqyzy@mail.ru>
2023-11-06 17:17:02 +0600

    login

--- Project/login.js
+++ Project/login.js
@@ -1,3 +1,42 @@
+document.getElementById("submit").addEventListener("click", function() {
+  // Get user input
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+
+  // Replace these values with your actual admin credentials
+  var adminEmail = "admin@example.com";
+  var adminPassword = "admin123";
+
+  // Check if the user input matches the admin credentials
+  if (email === adminEmail && password === adminPassword) {
+      // Redirect to the admin page
+      window.location.href = "Admin.html";
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // import { UserData } from './registration.js';
 
 // function isUserRegistered(name, password) {
